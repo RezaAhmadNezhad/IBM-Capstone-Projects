ba9e15fff1e1fc56d748777a6fdf7bfa29e9457b
RezaAhmadNezhad <127582731+RezaAhmadNezhad@users.noreply.github.com>
2023-07-19 14:42:26 +0200

    Update Colliecting data using Wb Scraping.py
    Read data and save in CSV file

--- Colliecting data using Wb Scraping.py
+++ Colliecting data using Wb Scraping.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup # this module helps in web scrapping.
 import requests  # this module helps us to download a web page
+import csv
 
 url = "http://www.ibm.com"
 # get the contents of the webpage in text format and store in a variable called data
@@ -35,7 +36,7 @@ url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DA
 data  = requests.get(url).text 
 soup = BeautifulSoup(data,"html5lib")  # create a soup object using the variable 'data'
 
-
+language_data = []
 #Scrape the `Language name` and `annual average salary`.
 table = soup.find('table') # in html table is represented by the tag <table>
 #Get all rows from the table
@@ -44,4 +45,13 @@ for row in table.find_all('tr'): # in html table row is represented by the tag <
     cols = row.find_all('td') # in html a column is represented by the tag <td>
     Language_name = cols[1].getText() # store the value in column 3 as color_name
     Annual_average_salary = cols[3].getText() # store the value in column 4 as color_code
-    print("{}--->{}".format(Language_name,Annual_average_salary))
+    language_data.append([Language_name, Annual_average_salary])
+
+# Save the data to a CSV file
+csv_filename = "programming_languages.csv"
+with open(csv_filename, mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["Language Name", "Annual Average Salary"])  # Write header
+    writer.writerows(language_data)
+
+print("Data saved to", csv_filename)
