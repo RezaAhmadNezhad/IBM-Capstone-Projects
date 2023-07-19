21dfbe84da69e7046e57af6f264f26efbed9a431
RezaAhmadNezhad <127582731+RezaAhmadNezhad@users.noreply.github.com>
2023-07-19 14:21:55 +0200

    Update Colliecting data using Wb Scraping.py

--- Colliecting data using Wb Scraping.py
+++ Colliecting data using Wb Scraping.py
@@ -27,3 +27,21 @@ for row in table.find_all('tr'): # in html table row is represented by the tag <
     color_name = cols[2].getText() # store the value in column 3 as color_name
     color_code = cols[3].getText() # store the value in column 4 as color_code
     print("{}--->{}".format(color_name,color_code))
+    
+    
+#this url contains the data you need to scrape
+url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DA0321EN-SkillsNetwork/labs/datasets/Programming_Languages.html"
+# get the contents of the webpage in text format and store in a variable called data
+data  = requests.get(url).text 
+soup = BeautifulSoup(data,"html5lib")  # create a soup object using the variable 'data'
+
+
+#Scrape the `Language name` and `annual average salary`.
+table = soup.find('table') # in html table is represented by the tag <table>
+#Get all rows from the table
+for row in table.find_all('tr'): # in html table row is represented by the tag <tr>
+    # Get all columns in each row.
+    cols = row.find_all('td') # in html a column is represented by the tag <td>
+    Language_name = cols[1].getText() # store the value in column 3 as color_name
+    Annual_average_salary = cols[3].getText() # store the value in column 4 as color_code
+    print("{}--->{}".format(Language_name,Annual_average_salary))
