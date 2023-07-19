d8193ed09e1125703b1d2e338287051e1ed577b7
RezaAhmadNezhad <127582731+RezaAhmadNezhad@users.noreply.github.com>
2023-07-19 17:05:05 +0200

    Update Explore the Data Set and create DataFrame.py

--- Explore the Data Set and create DataFrame.py
+++ Explore the Data Set and create DataFrame.py
@@ -37,6 +37,10 @@ print ('Duplicated values in Respondent Column are:', duplicate_in_Respondent)
 # Remove the duplicate rows from the dataframe.
 df.drop_duplicates(inplace=True)
 
+# Find the number of blank (missing or NaN) rows in the 'EdLevel' column
+blank_edlevel_rows = df['EdLevel'].isnull().sum()
+print("Number of blank rows in the 'EdLevel' column:", blank_edlevel_rows)
+
 # Find the missing values for all columns.
 missing_values = df.isnull().sum()
 print("Missing values for each column:")
@@ -46,6 +50,10 @@ print(missing_values)
 missing_values1 = df['WorkLoc'].isnull().sum()
 print("Missing values for WorkLoc column:", missing_values1)
 
+# Find out how many rows are missing in the column 'Country'
+missing_values2 = df['Country'].isnull().sum()
+print("Missing values for Country column:", missing_values2)
+
 # Find the value counts for the column WorkLoc.
 workloc_value_counts = df['WorkLoc'].value_counts()
 print("Value counts for the 'WorkLoc' column:", workloc_value_counts)
@@ -54,10 +62,25 @@ print("Value counts for the 'WorkLoc' column:", workloc_value_counts)
 most_frequent_workloc = df['WorkLoc'].value_counts().idxmax()
 print("The value that is most frequent in the 'WorkLoc' column:", most_frequent_workloc)
 
+# Identify the value that is most frequent (majority) in the Employment column.
+most_frequent_Employment = df['Employment'].value_counts().idxmax()
+print("The value that is most frequent in the 'Employment' column:", most_frequent_Employment)
+
+# Identify the value that is minimum in the UndergradMajor column.
+minimum_UndergradMajor = df['UndergradMajor'].value_counts().idxmin()
+print("The value that is minimum in the 'EUndergradMajor' column:", minimum_UndergradMajor)
 
 # Impute (replace) all the empty rows in the column WorkLoc with the value that you have identified as majority.
 # Impute (replace) empty rows in the 'WorkLoc' column with 'Office'
 df['WorkLoc'].fillna(most_frequent_workloc, inplace=True)
+
+# How many Respondent are being paid yearly?
+# Filter the DataFrame to include only respondents who are paid yearly
+yearly_paid_respondents = df[df['CompFreq'] == 'Yearly']
+# Count the number of respondents who are paid yearly
+num_yearly_paid_respondents = len(yearly_paid_respondents)
+print("Number of respondents being paid yearly:", num_yearly_paid_respondents)
+
 
 
 #### --------> Normalizing data
@@ -81,4 +104,9 @@ def calculate_normalized_compensation(row):
 # Apply the custom function to create the 'NormalizedAnnualCompensation' column
 df['NormalizedAnnualCompensation'] = df.apply(calculate_normalized_compensation, axis=1)
 print (df.head())
+
+
+# Find the median of the 'NormalizedAnnualCompensation' column
+median_normalized_compensation = df['NormalizedAnnualCompensation'].median()
+print("Median of NormalizedAnnualCompensation:", median_normalized_compensation)
 
